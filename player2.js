7a5f15172daba275613dcdcd02c94af6ede4809d
emnasop <asdopwin@outlook.com>
2025-10-05 16:22:30 +0800

    fix

--- player2.js
+++ player2.js
@@ -64,7 +64,7 @@ function highlightWords(currentTime) {
     for (let i = 0; i < currentLyric.etext.length; i++) {
         const word = currentLyric.etext[i];//简化m
         if (currentTime >= word.start && wordElements[i]) { //判断时间
-            wordElements[i].style.setProperty('animation:', 'fadeInUp 0.9s forwards;');
+            wordElements[i].style.setProperty('animation', 'fadeInUp 0.9s forwards;');
         } else if (currentTime < word.start && wordElements[i]) {
             wordElements[i].style.removeProperty('animation');
         }
